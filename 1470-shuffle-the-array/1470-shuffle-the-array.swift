867043c657032b030f4a1846339e0b4dbd61ed05
Omar R <oradwan037@gmail.com>
2022-05-15 02:45:50 +0200

    Time: 38 ms (48.35%), Space: 14.1 MB (83.52%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.swift
+++ 1470-shuffle-the-array/1470-shuffle-the-array.swift
@@ -2,7 +2,7 @@ class Solution {
     func shuffle(_ nums: [Int], _ n: Int) -> [Int] {
         var result : [Int] = []
         var n = n
-        for i in 0..<nums.count/2 {
+        for i in 0..<n {
             result.append(nums[i])
             result.append(nums[n])
             n += 1
