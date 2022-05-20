b3149657ddb407e42c45d620e823f28152adc162
Omar R <oradwan037@gmail.com>
2022-05-20 03:46:44 +0200

    Time: 69 ms (47.43%), Space: 14.4 MB (63.32%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.swift
+++ 905-sort-array-by-parity/905-sort-array-by-parity.swift
@@ -1,17 +1,14 @@
 class Solution {
     func sortArrayByParity(_ nums: [Int]) -> [Int] {
         guard nums.count > 0 else {return nums}
-        var result : [Int] = []
-        for num in nums {
-            if num % 2 == 0 {
-                result.append(num)
+        var nums = nums
+        var index = 0
+        for i in 0..<nums.count {
+            if nums[i] % 2 == 0 {
+                nums.swapAt(index,i)
+                index += 1
             }
         }
-        for num in nums {
-            if num % 2 != 0 {
-                result.append(num)
-            }
-        }
-        return result
+        return nums
     }
 }
