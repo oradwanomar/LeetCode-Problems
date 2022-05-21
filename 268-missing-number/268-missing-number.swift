e9bf6bbbe1858ffd2efb82b4e61b10fb128d188e
Omar R <oradwan037@gmail.com>
2022-05-21 12:14:57 +0200

    Time: 135 ms (52.34%), Space: 14.5 MB (47.27%) - LeetHub

--- 268-missing-number/268-missing-number.swift
+++ 268-missing-number/268-missing-number.swift
@@ -1,25 +1,13 @@
 class Solution {
     func missingNumber(_ nums: [Int]) -> Int {
-        var nums = nums
-        nums.sort()
-        for i in 0..<nums.count-1 {
-            if nums[i] + 1 != nums[i+1] {
-                return nums[i] + 1
-            }
-        }
-        if nums.count == 1 {
-            if nums[0] == 0 {
-                return 1
-            }else {
-               return 0 
-            }
-        }else {
-            if nums[0] == 0 {
-                  return nums[nums.count - 1] + 1
-            } else {
-                  return 0
-            }
-          
+       let sortedNum = nums.sorted()
+       for (index, num) in sortedNum.enumerated() {
+            if index != num {
+                return index
         }
     }
+    return nums.count
+    }
 }
+
+
