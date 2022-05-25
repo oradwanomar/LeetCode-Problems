d544981f4ea5da4e1a9c2d5e87c6db967cb26d9c
Omar R <oradwan037@gmail.com>
2022-05-25 05:01:17 +0200

    Time: 11 ms (68.00%), Space: 15 MB (48.00%) - LeetHub

---
+++ 2129-capitalize-the-title/2129-capitalize-the-title.swift
@@ -0,0 +1,34 @@
+class Solution {
+    func capitalizeTitle(_ title: String) -> String {
+        var title  = title.lowercased()
+        var array = title.components(separatedBy:" ")
+        var output = ""
+        var spaceCheck = array.count
+        for i in 0..<array.count {
+            if array[i].count > 2 {
+            array[i].capitalizeFirstLetter()
+            output.append(array[i])
+            if i != spaceCheck-1{
+                output.append(" ")
+            }
+            }else {
+                output.append(array[i])
+            if i != spaceCheck-1{
+                output.append(" ")
+            }
+            }
+            
+        }
+        return output
+    }
+}
+
+extension String {
+    func capitalizingFirstLetter() -> String {
+        return prefix(1).capitalized + dropFirst()
+    }
+
+    mutating func capitalizeFirstLetter() {
+        self = self.capitalizingFirstLetter()
+    }
+}
