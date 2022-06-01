7231961de9ea1ffecd221ff1bd3c1d7f876da6e9
Omar R <oradwan037@gmail.com>
2022-06-01 17:16:41 +0200

    Time: 24 ms (71.43%), Space: 14.6 MB (11.90%) - LeetHub

---
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
+        var counter = 0 
+        var temp = nums[0]
+        var result : [Int] = []
+        var dictionary = [Int:Int]()
+        for i in 0..<nums.count{
+           if dictionary[nums[i]] == nil {
+               dictionary[nums[i]] = 1
+               result.append(nums[i])
+           }else if dictionary[nums[i]] == 1{
+                  dictionary[nums[i]]! += 1
+                  result.append(nums[i])
+           }
+        }
+        nums = result
+        return nums.count
+    }
+}
