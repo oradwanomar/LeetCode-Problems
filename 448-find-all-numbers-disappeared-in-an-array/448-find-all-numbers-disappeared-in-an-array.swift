a81b005b95d3a78ab91d837ecc1215ebe81234b4
Omar R <oradwan037@gmail.com>
2022-06-03 17:25:58 +0200

    Time: 589 ms (26.00%), Space: 16.8 MB (24.00%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.swift
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.swift
@@ -2,13 +2,12 @@ class Solution {
     func findDisappearedNumbers(_ nums: [Int]) -> [Int] {
         var result : [Int] = []
         var dic = [Int:Int]()
-        var sortarr = nums.sorted()
-        for i in sortarr {
+        for i in nums {
             if dic[i] == nil {
                 dic[i] = i
             }
         }
-        for i in 1...sortarr.count {
+        for i in 1...nums.count {
             if dic[i] == nil {
                 result.append(i)
             }
