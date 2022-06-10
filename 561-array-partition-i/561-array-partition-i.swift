29ae1997747b4e9bc46e02e63a6b57a8fcc021d5
Omar R <oradwan037@gmail.com>
2022-06-10 20:11:28 +0200

    Time: 442 ms (32.88%), Space: 14.8 MB (67.12%) - LeetHub

---
+++ 561-array-partition-i/561-array-partition-i.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func arrayPairSum(_ nums: [Int]) -> Int {
+        guard nums.count > 1 else {return nums[0]}
+        var arr = nums.sorted()
+        var i = 0
+        var j = 1
+        var optimal = 0
+        while (i < nums.count - 1 && j < nums.count){
+            let min = min(arr[i],arr[j])
+            optimal += min
+            i += 2
+            j += 2
+        }
+        return optimal
+    }
+}
