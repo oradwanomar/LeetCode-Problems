eb861b3c44a446fd2a23bb03b59b0d774f765991
Omar R <oradwan037@gmail.com>
2022-06-12 03:18:11 +0200

    Time: 153 ms (55.95%), Space: 18.1 MB (70.24%) - LeetHub

---
+++ 204-count-primes/204-count-primes.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func countPrimes(_ n: Int) -> Int {
+        var count = 0
+        var primes = Array(repeating: true, count: n)
+        var i=2
+        
+        while i<n {
+            if primes[i] == true {
+                count += 1
+                var j=i*2
+                while(j<n) {
+                    primes[j] = false
+                    j+=i
+                }
+            }
+            i += 1
+        }
+        return count
+    }
+}
