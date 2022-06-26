e45fadb5326515a3fbf4250429306c898f3e3f21
Omar R <oradwan037@gmail.com>
2022-06-26 05:15:44 +0200

    Time: 155 ms (15.39%), Space: 14.6 MB (76.92%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.swift
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.swift
@@ -1,20 +1,22 @@
 class Solution {
     func nextGreatestLetter(_ letters: [Character], _ target: Character) -> Character {
-        var low = 0
-        var high = letters.count
-        
-        while (low < high) {
-            let mid = low + (high - low) / 2
-            
-            if target < letters[mid] {
-                high = mid
-            }
-            else {
-                low = mid + 1
-            }
+       var low = 0
+var high = letters.count - 1
+
+    while low < high {
+        let mid = low + (high - low) / 2
+        if letters[mid] > target {
+            high = mid
+        } else {
+            low = mid + 1
         }
-        
-        return letters[low % letters.count]
+    }
+
+    if letters[low] > target {
+        return letters[low]
+    }
+
+    return letters.first!
 
     }
 }
