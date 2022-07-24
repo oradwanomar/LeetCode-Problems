6a8904c7cc3be635dbf36e07e4ebe286dedcebb3
Omar R <oradwan037@gmail.com>
2022-07-24 12:54:43 +0200

    Time: 41 ms (34.34%), Space: 14.4 MB (53.17%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.swift
@@ -0,0 +1,25 @@
+class Solution {
+    func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right = nums.count - 1
+        
+        while left <= right {
+            let mid = (left + right) / 2
+            
+            if nums[mid] == target{
+                return mid
+            }
+            if nums[mid] < target {
+                left = mid + 1
+            }else {
+                right = mid - 1
+            }
+        }
+        return left
+        // if nums[left] < target {
+        //     return left 
+        // }else {
+        //     return right 
+        // }
+    }
+}
