a46f68f8acaebd121cac8fcefe73c5aa347dcf81
Omar R <oradwan037@gmail.com>
2022-07-24 12:56:28 +0200

    Time: 559 ms (6.19%), Space: 14.6 MB (97.94%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.swift
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.swift
@@ -1,15 +1,17 @@
 class Solution {
     func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
-        var row = matrix.count - 1, col = 0
-        while row >= 0 && col < matrix[0].count {
-            if matrix[row][col] < target {
-                col += 1
-            } else if matrix[row][col] > target {
-                row -= 1
-            } else {
-                return true
-            }
-        }
-        return false
+       var row = matrix.count - 1
+       var col = 0
+       while row >= 0 && col < matrix[0].count {
+           if matrix[row][col] < target {
+               col += 1
+           }
+           else if matrix[row][col] > target {
+               row -= 1
+           }else {
+               return true
+           }
+       }
+        return false 
     }
 }
